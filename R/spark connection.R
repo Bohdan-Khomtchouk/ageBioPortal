4392bc2d68d7fc638ec3154e566ecc86a4d13c95
William Koehler <williamk03hler@gmail.com>
2018-04-13 19:45:13 -0700

    FIXED merge

--- R/spark connection.R
+++
@@ -1,8 +0,0 @@
-#'
-#'
-#'
-#'@import sparklyr
-#'@import dplyr
-check_connection <- function(x) {
-  sc <- spark_connect(master = 'local"')
-}
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(ageBioPortalclient)
-
-test_check("ageBioPortalclient")
